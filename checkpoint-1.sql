7883d2e04ce6850c58bfe1ce6f2cd7d26b6abd7f
aynur-dursunova <dursunovaaynur@gmail.com>
2026-07-19 19:31:39 +0400

    Update checkpoint-1.sql

--- checkpoint-1.sql
+++ checkpoint-1.sql
@@ -1,1 +1,31 @@
+--1)Bütün müştəriləri göstər
+select * from Customers;
 
+--Customers cədvəlindən bütün müştərilərin CompanyName, ContactName və City sütunlarını göstər
+select CompanyName, ContactName, City
+from Customers;
+
+--2)Yalnız Kanadadakı müştəriləri göstər
+select * from Customers
+where Country = 'Canada';
+
+--Products cədvəlindən qiyməti 35-dən böyük olan məhsulların ProductName və UnitPrice sütunlarını göstər
+select ProductName, UnitPrice
+from Products
+where UnitPrice > 35;
+
+--3)Products cədvəlindəki bütün məhsulları UnitPrice sütununa görə azalan qaydada sırala
+select * from Products
+order by UnitPrice DESC;
+
+--4)Products cədvəlindən qiyməti 10-dan böyük olan məhsulları qiymətə görə azalan sırala və yalnız ilk 7 məhsulu göstər
+select *
+from Products
+where UnitPrice > 10
+order by UnitPrice DESC
+limit 7;
+
+--5)Customers cədvəlindən yalnız Berlin şəhərində yerləşən müştərilərin CompanyName, City və Country sütunlarını göstər
+select CompanyName, City, Country
+from Customers
+where City = 'Berlin';
